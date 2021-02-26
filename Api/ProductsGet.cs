73cc5a4fa250937a742eb5b34de601575ec2173c
teberl <mail@teberl.de>
2021-02-26 14:45:50 +0100

    Add ProductsGet function to api

---
+++ Api/ProductsGet.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Api
+{
+    public class ProductsGet
+    {
+        private readonly IProductData productData;
+
+        public ProductsGet(IProductData productData)
+        {
+            this.productData = productData;
+        }
+
+        [FunctionName("ProductsGet")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function ProductsGet processed a request.");
+
+            var products = this.productData.GetProducts();
+            return new ObjectResult(products);
+        }
+    }
+}
